9c3b97ea87faade288eecaca28fde6641be98c36
genral-walker <lukmansanni60@gmail.com>
2023-07-31 13:08:01 +0100

    updates

--- src/pages/Product/ProductList/index.tsx
+++ src/pages/Product/ProductList/index.tsx
@@ -69,13 +69,6 @@ export default class ProductList extends Component {
     this.fetchProducts();
   }
 
-  componentDidUpdate() {
-    const { setProducts, products } = this.context as AppContextType;
-    if (!this.state.deleteLoading && !products.length) {
-      setProducts([]);
-    }
-  }
-
   render() {
     const { products } = this.context as AppContextType;
     return (
